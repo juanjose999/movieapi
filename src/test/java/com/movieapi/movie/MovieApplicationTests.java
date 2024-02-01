7e344ff3f9e0cf724c2afd162ea3a9d90a3d22d8
juanjose999 <juanjose000012@gmail.com>
2024-02-01 15:04:15 -0500

    proyect inicializer

---
+++ src/test/java/com/movieapi/movie/MovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movieapi.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
